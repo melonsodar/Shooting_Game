bd7b751a5ea2b75f6b6c41cb67df73b9a85e0be1
hw16a108 <hw16a108@oecu.jp>
2017-11-28 16:22:51 +0900

    C完了！

--- Sources/Game.cpp
+++ Sources/Game.cpp
@@ -22,8 +22,8 @@ int     score;          //!< スコア
 void Start()
 {
     cloudPos = Vector2(-320, 100);
-    cannonPos = Vector2(-80, -150);
-    targetRect = Rect(80, -140, 40, 40);
+    cannonPos = Vector2(-310, -150);//(C HW16A108 相馬　砲台を左に)
+    targetRect = Rect(280, -140, 40, 40);//(C HW16A108 相馬　ターゲットを右に)
     bulletPos.x = -999;
     score = 0;
 }
